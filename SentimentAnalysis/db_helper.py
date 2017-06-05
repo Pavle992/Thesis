6ae579b74a6aa6468b3baebd7c6e8f89c5ed9ef0
Pavle992 <pavle.vidanovic@gmail.com>
2017-06-05 18:01:34 +0200

    dbhelper

---
+++ SentimentAnalysis/db_helper.py
@@ -0,0 +1,27 @@
+import pymysql
+
+hostname = 'localhost'
+username = 'root'
+password = 'Koliko90'
+database = 'im_dump'
+
+def doQuery(conn, queryText) :
+    cur = conn.cursor()
+	cur.execute(queryText)
+	print("Executin query: ", queryText)
+	return cur
+    # for cnt in cur.fetchall() :
+    #     print(cnt)
+
+def db_connect() :
+	myConnection = pymysql.connect( host=hostname, user=username, passwd=password, db=database )
+	print("Connected to DB..")
+	return myConnection
+
+def db_close(conn):
+	conn.close()
+	print("Closed DB connection..")
+
+
+
+
