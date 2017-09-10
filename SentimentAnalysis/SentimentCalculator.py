9cb51af2df0415ace740d9a1eefde78d55abda9a
Pavle992 <pavle.vidanovic@gmail.com>
2017-09-10 12:05:01 +0200

    Spam detection function created and tested in proba notebook

--- SentimentAnalysis/SentimentCalculator.py
+++ SentimentAnalysis/SentimentCalculator.py
@@ -113,5 +113,12 @@ class SentimentCalculator:
 		print(summedSent)
 		return summedSent
 
+	def checkSpam(commentText):
+	    spam_pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+	    result = re.findall(spam_pattern, commentText)
+	    if len(result) == 0:
+	        return False
+	    else:
+	        return True
 	
 
